bab3ac9af526bc891345b0c2f1f9aeeb15a796f9
Nyans2022 <45394200+Lonely-people@users.noreply.github.com>
2024-10-18 20:17:09 +0800

    Delete hw1-2 directory

--- hw1-2/1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	int h; scanf("%d", &h);
-	double ans = (h - 100) * 0.9 * 2;
-	printf("%.1lf\n", ans);
-	return 0;
-}
--- hw1-2/2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-
-int main() {
-	double m, y, r; scanf("%lf%lf%lf", &m, &y, &r);
-	double i = m * pow(1 + r, y) - m;
-	printf("interest = %.2lf\n", i);
-	return 0;
-}
