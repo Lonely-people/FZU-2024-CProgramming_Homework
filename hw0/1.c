8613b2010c3a290af77fd6fe33eb61620135d619
Nyans2022 <45394200+Lonely-people@users.noreply.github.com>
2024-10-18 20:17:01 +0800

    Delete hw0 directory

--- hw0/1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	for (int i = 1; i <= 3; ++i) {
-		printf("I'm gonna WIN!\n");
-	}
-	return 0;
-}
--- hw0/3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	int a, b;
-	scanf("%d%d", &a, &b);
-	printf("C = %d\n", (a + b) * 2);
-	printf("S = %d\n", a * b);
-	return 0;
-}
