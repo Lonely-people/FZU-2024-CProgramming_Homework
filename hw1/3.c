74aaa507d24fe1c166afaf9ec3b627d274d6946b
Nyans2022 <45394200+Lonely-people@users.noreply.github.com>
2024-10-18 20:17:16 +0800

    Delete hw1 directory

--- hw1/3.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	int y, m, d;
-	scanf("%d-%d-%d", &m, &d, &y);
-	printf("%d-%02d-%02d", y, m, d);
-	return 0;
-}
--- hw1/4.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	int a, t, b; scanf("%d%d%d", &a, &t, &b);
-	long long ans = 1.0 * t * (b - 1) / (a - 1);
-	printf("%lld\n", ans);
-	return 0;
-}
